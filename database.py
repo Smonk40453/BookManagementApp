b95fba080c57b34f422b881c097af70b9db24c90
username <useremail>
2026-08-09 10:13:22 -0700

    Add initial SQLite DB Setup

---
+++ database.py
@@ -0,0 +1,17 @@
+import sqlite3 
+connection = sqlite3.connect("book_managment.db")
+cursor = connection.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS books(
+               book_id INTEGER PRIMARY KEY AUTOINCREMENT,
+               title TEXT NOT NULL,
+               author TEXT NOT NULL,
+               reading_status TEXT NOT NULL,
+               rating INTEGER,
+               notes TEXT
+    )
+""")
+connection.commit()
+print("Books table created successfully!")
+connection.close()
