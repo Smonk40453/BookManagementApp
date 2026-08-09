c243b0ad9db17d19afb9d380d22fb5e4c0056517
username <useremail>
2026-08-09 11:10:59 -0700

    add PUT API endpoint for updating books

--- app.py
+++ app.py
@@ -35,4 +35,35 @@ def add_book():
     return jsonify({
         "message": "Book added successfully!",
         "book_id": book_id 
-    }), 201          
+    }), 201    
+@app.route ("/books/<int:book_id>",methods=["PUT"])
+def update_book(book_id):
+    data=request.get_json()
+    title = data.get("title")
+    author = data.get("author")
+    reading_status = data.get("reading_status")
+    rating = data.get("rating")
+    notes  = data.get("notes")
+    if not title or not author or not reading_status:
+        return jsonify({"error": "Title, author, and reading status are required."}), 400 
+    connection = sqlite3.connect("book_managment.db")
+    cursor = connection.cursor()
+    cursor.execute("""
+        UPDATE books
+        SET title = ?, author = ?, reading_status = ?, rating = ?, notes = ?
+        WHERE book_id = ?
+    """,(
+        title, 
+        author, 
+        reading_status,
+        rating, 
+        notes,
+        book_id
+    ))
+    connection.commit()
+    if cursor.rowcount == 0:
+        connection.close()
+        return jsonify({"error": "Book note found."}),404
+    connection.close()
+    return jsonify({
+        "message": "Book updated successfully!"}),200
