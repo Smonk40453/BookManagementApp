30d75a8440884179d2fe6c611025c182e5038d60
username <useremail>
2026-08-09 10:47:39 -0700

    Add POST books API endpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify,request
 import sqlite3
 
 app = Flask(__name__)
@@ -9,10 +9,30 @@ def home():
 def get_books():
     connection = sqlite3.connect("book_managment.db")
     cursor = connection.cursor()
-
     cursor.execute("SELECT * FROM books")
     books = cursor.fetchall()
-
     connection.close()
-
     return jsonify(books)
+@app.route("/books",methods=["POST"])
+def add_book():
+    data = request.get_json()
+    title = data.get("title")
+    author = data.get("author")
+    reading_status = data.get("reading_status")
+    rating = data.get("rating")
+    notes = data.get("notes")
+    if not title or not author or not reading_status:
+        return jsonify({"error": "Title, author, and reading status are required."}), 400
+    connection = sqlite3.connect("book_managment.db")
+    cursor = connection.cursor()
+    cursor.execute("""
+        INSERT INTO books (title, author, reading_status, rating, notes)
+        VALUES (?,?,?,?,?)
+    """,(title, author, reading_status, rating, notes))
+    connection.commit()
+    book_id = cursor.lastrowid
+    connection.close()
+    return jsonify({
+        "message": "Book added successfully!",
+        "book_id": book_id 
+    }), 201          
