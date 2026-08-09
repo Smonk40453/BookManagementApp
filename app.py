5e2836064f1b6c320e2b3ba7c749b67822112739
username <useremail>
2026-08-09 10:26:46 -0700

    add GET books API endpoint

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
-from flask import Flask 
+from flask import Flask, jsonify
+import sqlite3
+
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "Book Managment App is running!"
+    return "Book Management App is running!"
+@app.route("/books", methods=["GET"])
+def get_books():
+    connection = sqlite3.connect("book_managment.db")
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM books")
+    books = cursor.fetchall()
+
+    connection.close()
+
+    return jsonify(books)
